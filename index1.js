35e9fa4461455e856ad0b303bceffcfcc10d8066
Brian Ali <gamerfunky78@gmail.com>
2024-06-12 13:02:47 +0300

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,12 @@
+function user_handle(){
+
+    let email = document.getElementById("display").value
+
+    toString(email.value)
+
+    document.getElementById("linkers").innerHTML = "Your Email : " + email + " has been recieved. stay tuned." 
+
+
+
+
+} 
